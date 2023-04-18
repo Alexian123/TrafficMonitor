371c6d9a3163426c5ceaf98b625a22ac15afc1fe
Alexian Hentiu <alexianhentiu1208@gmail.com>
2023-04-18 20:14:52 +0300

    TrafficLight library

---
+++ libraries/TrafficLight/TrafficLight.cpp
@@ -0,0 +1,87 @@
+#include "Arduino.h"
+#include "TrafficLight.h"
+
+#define BIT0 0x1
+#define BIT1 0x2
+#define BIT2 0x4
+
+using namespace TrafficMonitor;
+
+TrafficLight::TrafficLight(byte rPin, byte gPin, byte yPin)
+    : rPin(rPin), gPin(gPin), yPin(yPin)
+{
+}
+
+TrafficLight::~TrafficLight()
+{
+}
+
+void TrafficLight::run()
+{
+    if (blinkMode) {
+        blink();
+    } else {
+        light(state);
+    }
+}
+
+void TrafficLight::light(TLState newState) const
+{
+    digitalWrite(rPin, newState & BIT0);
+    digitalWrite(gPin, newState & BIT1);
+    digitalWrite(yPin, newState & BIT2);
+}
+
+void TrafficLight::blink()
+{
+    long currentMillis = millis();
+    if ((ledState == HIGH) && (currentMillis - lastMillis >= onTime)) {
+        ledState = LOW;
+        lastMillis = currentMillis;
+        light(TLState::OFF);
+    } else if ((ledState == LOW) && (currentMillis - lastMillis >= offTime)) {
+        ledState = HIGH;
+        lastMillis = currentMillis;
+        light(state);
+    }
+}
+
+void TrafficLight::setState(TLState state)
+{
+    this -> state = state;
+}
+
+TLState TrafficLight::getState() const
+{
+    return state;
+}
+
+void TrafficLight::setBlinkMode(bool blinkMode)
+{
+    this -> blinkMode = blinkMode;
+}
+
+bool TrafficLight::getBlinkMode() const
+{
+    return blinkMode;
+}
+
+void TrafficLight::setOnTime(int onTime)
+{
+    this -> onTime = onTime;
+}
+
+int TrafficLight::getOnTime() const
+{
+    return onTime;
+}
+
+void TrafficLight::setOffTime(int offTime)
+{
+    this -> offTime = offTime;
+}
+
+int TrafficLight::getOffTime() const
+{
+    return offTime;
+}
---
+++ libraries/TrafficLight/TrafficLight.h
@@ -0,0 +1,53 @@
+#ifndef TRAFFIC_LIGHT_H
+#define TRAFFIC_LIGHT_H
+
+#include "Arduino.h"
+
+namespace TrafficMonitor
+{
+    enum TLState
+    {
+        OFF     = 0x0,    // 0b000
+        RED     = 0x1,    // 0b001
+        GREEN   = 0x2,    // 0b010
+        YELLOW  = 0x4,    // 0b100
+    };
+
+    class TrafficLight
+    {
+    private:
+        const byte rPin;
+        const byte gPin;
+        const byte yPin;
+
+        TLState state = OFF;
+        bool blinkMode = false;
+
+        // blink timing
+        byte ledState = LOW;
+        int onTime = 1000;  // ms
+        int offTime = 1000; // ms
+        long lastMillis = 0; 
+    public:
+        TrafficLight(byte rPin, byte gPin, byte yPin);
+        ~TrafficLight();
+
+        void run(); // call inside loop
+
+        // Getters & setters
+        void setState(TLState state);
+        TLState getState() const;
+        void setBlinkMode(bool blinkMode);
+        bool getBlinkMode() const;
+        void setOnTime(int onTime);
+        int getOnTime() const;
+        void setOffTime(int offTime);
+        int getOffTime() const;
+
+    private:
+        void light(TLState newState) const; // constant light
+        void blink();                       // blinking light
+    };
+}
+
+#endif
